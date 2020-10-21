fd5a9522c23ce47eb987c74f450199bb2e8600f5
Anisha Tripathi <anishatripathi@anishas-air.lan>
2020-10-21 00:08:17 -0700

    html adjustments

--- embedder.js
+++ embedder.js
@@ -1,5 +1,5 @@
 const guessing_img = 'https://www.google.com/maps/embed?pb=!4v1603261479820!6m8!1m7!1sCAoSLEFGMVFpcFBLU1JFYUU4QjJVOXpNQlZOdm5sa1BnendSUldpaG5hbWhxd2FS!2m2!1d33.6449944!2d-117.8422998!3f103.28060675164178!4f0!5f0.7820865974627469';
-const pic_container = document.querySelector(".guess-picture");
+//const pic_container = document.querySelector(".guess-picture");
 const pic_iframe = document.querySelector(".guess-picture iframe");
 
 function insert_url(){
