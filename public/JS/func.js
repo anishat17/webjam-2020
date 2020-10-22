9cb6d976f172c5d3a7ba6ef3bf88bd314094f7c4
Sahil Jagad <sjagad@uci.edu>
2020-10-22 00:33:31 -0700

    change background when start Pressed

--- public/JS/func.js
+++ public/JS/func.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     var x = document.getElementById("pop-up");
+    var y = document.getElementById("img_container")
     function hideWindow(){
         x.style.display = "none";
     }
@@ -8,8 +9,7 @@ $(document).ready(function(){
     }
     $('#close-popup').click(function(){
         hideWindow();
-        $(body).css("background","none");
-
-
+        y.style.background = "url(media/hess-13-irvine.jpg)";
+        y.style.animation = "none";
     })
 })
