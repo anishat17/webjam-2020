bb31673ca4a1a9b9b1c59cc46c70b30942289778
Sahil Jagad <sjagad@uci.edu>
2020-10-22 04:00:11 -0700

    added countdown timer

--- public/JS/app.js
+++ public/JS/app.js
@@ -12,5 +12,25 @@ $(document).ready(function(){
         y.style.background = "url(media/hess-13-irvine.jpg)";
         y.style.animation = "none";
     })
+
+    var yourDateToGo = new Date(); 
+    yourDateToGo.setDate(yourDateToGo.getDate() + 1);
+
+    var timing = setInterval( 
+      function () {
+
+        var currentDate = new Date().getTime(); 
+        var timeLeft = yourDateToGo - currentDate;
+
+        var seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+        if (seconds < 10) seconds="0"+seconds;
+
+        document.getElementById("countdown").innerHTML = seconds + " seconds left";
+
+        if (seconds == 0) {
+          clearInterval(timing);
+          document.getElementById("countdown").innerHTML = "Time's Up!";
+        }
+      }, 1000);
 })
 
