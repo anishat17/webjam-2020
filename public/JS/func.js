d9e0f052318c49d4f80a2aeb95bb1988a1536ed7
Mahika Popli <mahikapopli@Mahikas-MacBook-Air.local>
2020-10-22 00:02:55 -0700

    func.js updated

--- public/JS/func.js
+++ public/JS/func.js
@@ -3,12 +3,18 @@ $(document).ready(function(){
     function hideWindow(){
         x.style.display = "none";
     }
-    function showElements(){
-        
-    }
+    function change_background()
+        {
+            $("#img_container").hide();
+            //display(URL("css/red.jpeg"));
+            $("#bgd").show();
+            //$().css({"background-color": 'red', "z-index": "2"});
+           // $("#img_container").css("background,", 'url("https://www.abc.net.au/radionational/image/6289622-4x3-340x255.png")');
+           // $("img_container").css("animation,", 'none');
+
+        }
     $('#close-popup').click(function(){
         hideWindow();
-
-
-    })
-})
+        change_background();
+    });
+});
