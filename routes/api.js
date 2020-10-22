ff3f3f28952d21c9471ccf5ca063525273459cc0
Akhil Dhanala <akhil.dhanala@gmail.com>
2020-10-22 01:02:54 -0700

    minor changes to files

--- routes/api.js
+++ routes/api.js
@@ -7,7 +7,7 @@ const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: "heroku_3c5393be4190d25"
+    database: process.env.DB_NAME
 });
 
 db.connect((err) => {
