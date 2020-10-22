aca3c7282ca41b41201976552e133fe68396be69
Sahil Jagad <sjagad@uci.edu>
2020-10-22 00:48:19 -0700

    func.js working

--- public/JS/func.js
+++ public/JS/func.js
@@ -4,28 +4,12 @@ $(document).ready(function(){
     function hideWindow(){
         x.style.display = "none";
     }
-    function change_background()
-        {
-            $("#img_container").hide();
-            //display(URL("css/red.jpeg"));
-            $("#bgd").show();
-            //$().css({"background-color": 'red', "z-index": "2"});
-           // $("#img_container").css("background,", 'url("https://www.abc.net.au/radionational/image/6289622-4x3-340x255.png")');
-           // $("img_container").css("animation,", 'none');
-
-        }
+    function showElements(){
+        
+    }
     $('#close-popup').click(function(){
         hideWindow();
-<<<<<<< HEAD
         y.style.background = "url(media/hess-13-irvine.jpg)";
         y.style.animation = "none";
-=======
-        change_background();
-    });
-});
-        $(body).css("background","none");
-
-
->>>>>>> c2cf990db2be1ac82fab9cc16232f20aa18bf7a9
     })
 })
