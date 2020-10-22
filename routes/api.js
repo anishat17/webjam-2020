099b9b370744d1f6cf54ba87ed84e4deccc991b1
Akhil Dhanala <akhil.dhanala@gmail.com>
2020-10-21 23:55:06 -0700

    added a check answer route to api

--- routes/api.js
+++ routes/api.js
@@ -27,7 +27,7 @@ router.get("/maps/range", (req, res) => {
             res.status(503).send("503 Service Unavailable");
             console.log(err);
         }
-        res.send(result);
+        res.send(result[0]);
     });
 })
 
@@ -37,13 +37,35 @@ router.get("/maps", (req, res) => {
         res.status(400).send("Bad Request");
     }
     
-    db.query(`SELECT MapId, Location, Link FROM maps WHERE Mapid =${req.query.mapid}`, (err, result, fields) => {
+    db.query(`SELECT MapId, Link FROM maps WHERE Mapid =${req.query.mapid}`, (err, result, fields) => {
         if (err) {
             res.status(503).send("503 Service Unavailable");
         }
-        res.send(result);
+        res.send(result[0]);
     });
 
+});
+
+router.get("/maps/check", (req, res) => {
+    if (!req.query.mapid || !req.query.location) {
+        console.log("invalid request");
+        res.status(400).send("Bad Request");
+    }
+    let sql = `SELECT Mapid, Link FROM maps WHERE Mapid = ${req.query.mapid} AND Location = '${req.query.location}'`;
+    db.query(sql, (err, result, fields) => {
+        if (err) {
+            res.status(503).send("503 Service Unavailable");
+            console.log(err);
+        } else {
+            let data = {correct : false};
+            if (result.length == "0") {
+                res.send(data);
+            } else {
+                data.correct = true;
+                res.send(data);
+            }
+        }
+    });
 });
 
 module.exports = router;
@@ -51,6 +73,7 @@ module.exports = router;
 
 
 function trivialQuery() {
+    // keeps database connection from timing out
     console.log("looped");
     db.query("SELECT Mapid FROM maps WHERE Mapid < 0", (err, result, fields) => {
         if(err) throw err;
