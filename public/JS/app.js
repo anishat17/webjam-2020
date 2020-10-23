f9587e05b4508b839e907d4f55d6f178dde52bf9
Anisha Tripathi <anishatripathi@anishas-air.lan>
2020-10-23 01:39:51 -0700

    ui improvements

--- public/JS/app.js
+++ public/JS/app.js
@@ -13,12 +13,14 @@ $(document).ready(function(){
     function mapiframe(){
       initializeRound(range).then(data=>{
         console.log(data["link"]);
-        $(".map-container").append(`<iframe src="${data["link"]}" allowfullscreen="false"></iframe>`);
+        $(".map-container").append(`<iframe src="${data["link"]}" allowfullscreen="false" id="${data["Mapid"]}"></iframe>`);
         $(data["locations"]).each(function(index){
           $(`option[value='${index+1}']`).html(this);
         });
       });
     }
+
+
 
     $('#start-button').click(function(){
         hideWindow();
@@ -28,6 +30,23 @@ $(document).ready(function(){
         $(".map-container").removeClass("hide");
     })
 
+    $("#guess-form").submit(event => {
+      event.preventDefault();
+      var mapId = $(".map-container iframe").attr("id");
+      var option = $("#guess-dropdown option:selected").text();
+      checkOption(mapId, option).then(correct => {
+        if (correct){
+          //increment score by one!
+          mapiframe();
+        }
+        else{
+          //incorrect ans popup, call iframe again
+          console.log('incorrect');
+        }
+      })
+    })
+
+
     var clock = document.getElementById("countdown");
     var seconds = 60;
 
@@ -36,6 +55,8 @@ $(document).ready(function(){
         clock.innerHTML = seconds + " seconds left";
         seconds = seconds - 1;
     }
+
+    
 
     setInterval(timer, 1000);
 }); 
