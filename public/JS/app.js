6accd61d48e5e5a0b1650a172b075b97ebcb7151
Akhil Dhanala <akhil.dhanala@gmail.com>
2020-10-23 02:43:16 -0700

    integrated timer with game

--- public/JS/app.js
+++ public/JS/app.js
@@ -11,11 +11,12 @@ $(document).ready(function(){
     });
   
     function mapiframe(){
-      initializeRound(range).then(data=>{
+      return initializeRound(range).then(data=>{
         $(".map-container iframe").replaceWith(`<iframe src="${data["link"]}" allowfullscreen="false" id="${data["mapid"]}"></iframe>`);
         $(data["locations"]).each(function(index){
           $(`option[value='${index+1}']`).html(this);
         });
+        seconds = 60;
       });
     }
 
@@ -25,8 +26,10 @@ $(document).ready(function(){
         hideWindow();
         y.style.background = "url(media/hess-13-irvine.jpg)";
         y.style.animation = "none";
-        mapiframe();
-        $(".map-container").removeClass("hide");
+        mapiframe().then(() => {
+          $(".map-container").removeClass("hide");
+          countdown = setInterval(timer, 1000);
+        });
     })
 
     $("#guess-form").submit(event => {
@@ -37,14 +40,23 @@ $(document).ready(function(){
       checkOption(mapId, option).then(correct => {
         if (correct){
           //increment score by one!
-          mapiframe();
         }
         else{
           //incorrect ans popup, call iframe again
           console.log('incorrect');
         }
-      })
-    })
+        mapiframe();
+      });
+    });
+
+    $("#countdown").on("timeup", e => {
+      clock.innerHTML = "Time's Up!";
+      clearInterval(countdown);
+      setTimeout(() => {console.log("switching map");}, 2000);
+      mapiframe().then(() => {
+        countdown = setInterval(timer, 1000);
+      });
+    });
 
 
     var clock = document.getElementById("countdown");
@@ -54,9 +66,9 @@ $(document).ready(function(){
         if (seconds < 10) seconds= "0" + seconds;
         clock.innerHTML = seconds + " seconds left";
         seconds = seconds - 1;
+        if (seconds === 0) {
+          $("#countdown").trigger("timeup");
+        }
     }
 
-    
-
-    setInterval(timer, 1000);
 }); 
